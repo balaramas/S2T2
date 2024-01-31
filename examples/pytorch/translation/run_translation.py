5a0c1baf9c87f56adaaea49210c061848527c1e2
Balaram Sarkar <balarakar@gmail.com>
2024-01-31 15:27:36 +0530

    Update run_translation.py

--- examples/pytorch/translation/run_translation.py
+++ examples/pytorch/translation/run_translation.py
@@ -483,10 +483,10 @@ def main():
         inputs = [ex[source_lang] for ex in examples["translation"]]
         targets = [ex[target_lang] for ex in examples["translation"]]
         inputs = [prefix + inp for inp in inputs]
-        model_inputs = tokenizer(inputs, max_length=data_args.max_source_length, padding=padding, truncation=True)
+        model_inputs = tokenizer(inputs, src_lang="eng", max_length=data_args.max_source_length, padding=padding, truncation=True)
 
         # Tokenize targets with the `text_target` keyword argument
-        labels = tokenizer(text_target=targets, max_length=max_target_length, padding=padding, truncation=True)
+        labels = tokenizer(text_target=targets, src_lang="hin", max_length=max_target_length, padding=padding, truncation=True)
 
         # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
         # padding in the loss.
